74b25319a1d734963b68929b8bdccb635d2e4062
jayceleste-dewise <72493437+jayceleste-dewise@users.noreply.github.com>
2020-10-08 21:55:21 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+const config = require('config');
+const config = require('config');
+const config = require('config');
 const connectDB = async () => {
 	try {
 		await mongoose.connect(db, {
